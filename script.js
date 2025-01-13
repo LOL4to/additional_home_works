642c49e85654e3c08d6b54c38b93b384a297d86b
Ildar Khamzin <idl.08@mail.ru>
2025-01-13 20:19:48 +0900

    used method reduce

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ for (let i = 0; i < String(num).length; i++) {
   // число num в строку, затем перебираем каждую цифру
   numHelp *= Number(String(num)[i]); // переумножаем каждую цифру и переводим тип переменной обратно из string в number
 }
+console.log(
+  "Мой 1й способ с циклом for: " + String(numHelp ** 3).substring(0, 2)
+);
 
-console.log(String(numHelp ** 3).substring(0, 2));
+// 2й способ решения задачи, с помощью метода reduce:
+let numArray = String(num).split("").map(Number); // преобразуем число в строку, разделим на массив,
+// и преобразуем обратно в число, получив массив из чисел
+let numMulti = numArray.reduce(function (initial, current) {
+  // initial = a[0]
+  return initial * current;
+});
+console.log("2й способ с reduce: " + String(numMulti ** 3).substring(0, 2));
