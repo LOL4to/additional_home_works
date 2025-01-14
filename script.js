112107153c0ae4614f5260b446182d402c3dda56
Ildar Khamzin <idl.08@mail.ru>
2025-01-15 02:17:11 +0900

    added Additional lesson03

--- script.js
+++ script.js
@@ -1,8 +1,93 @@
-let num = 266219;
-let numHelp = 1; // для начального значения и помощи в цикле
-for (let i = 0; i < String(num).length; i++) {
-  // число num в строку, затем перебираем каждую цифру
-  numHelp *= Number(String(num)[i]); // переумножаем каждую цифру и переводим тип переменной обратно из string в number
+"use strict";
+
+// используя if-else:
+let lang = prompt("Выберите язык:", "ru, en");
+if (lang == "ru") {
+  console.log(
+    "Понедельник\n",
+    "Вторник\n",
+    "Среда\n",
+    "Четверг\n",
+    "Пятница\n",
+    "Суббота\n",
+    "Воскресенье"
+  );
+} else if (lang == "en") {
+  console.log(
+    "Monday\n",
+    "Tuesday\n",
+    "Wednesday\n",
+    "Thursday\n",
+    "Friday\n",
+    "Saturday\n",
+    "Sunday"
+  );
+} else {
+  console.log("Выберите 'ru' или 'en'");
 }
 
-console.log(String(numHelp ** 3).substring(0, 2));
+// используя switch:
+switch (lang) {
+  case "ru":
+    console.log(
+      "Понедельник\n",
+      "Вторник\n",
+      "Среда\n",
+      "Четверг\n",
+      "Пятница\n",
+      "Суббота\n",
+      "Воскресенье"
+    );
+    break;
+  case "en":
+    console.log(
+      "Monday\n",
+      "Tuesday\n",
+      "Wednesday\n",
+      "Thursday\n",
+      "Friday\n",
+      "Saturday\n",
+      "Sunday"
+    );
+    break;
+  default:
+    console.log("Выберите 'ru' или 'en'");
+}
+
+// используя многомерный массив:
+let langArr = lang.split();
+let myArr = [
+  [
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье",
+  ],
+  [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ],
+];
+
+String(langArr) == ["ru"] || String(langArr).includes("en") == true // использ. 2 способа сравн для интереса
+  ? String(langArr) == ["ru"]
+    ? console.log(myArr[0])
+    : console.log(myArr[1])
+  : console.log("Выберите ru или en!");
+
+//----// следующая задачка
+let namePerson = prompt("Введите имя!");
+console.log(namePerson);
+namePerson === "Артем" || namePerson === "Александр"
+  ? namePerson === "Артем"
+    ? console.log("директор")
+    : console.log("преподаватель")
+  : console.log("студент");
