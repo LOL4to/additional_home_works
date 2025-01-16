5ccf7abef983325e7bc8a336e6a6890b2d65e6f6
Ildar Khamzin <idl.08@mail.ru>
2025-01-17 04:35:40 +0900

    added Addiional lesson04

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let num = 266219;
-let numHelp = 1; // для начального значения и помощи в цикле
-for (let i = 0; i < String(num).length; i++) {
-  // число num в строку, затем перебираем каждую цифру
-  numHelp *= Number(String(num)[i]); // переумножаем каждую цифру и переводим тип переменной обратно из string в number
-}
+"use strict";
 
-console.log(String(numHelp ** 3).substring(0, 2));
+let myVar = "Эта строка займёт 29 символов";
+const testVar = function (varCheck, callback) {
+  return console.log(
+    typeof varCheck === "string" ? callback(varCheck) : "это не строка" // Если в качестве аргумента передана не строка - функция оповещает об этом пользователя
+  );
+};
+const strFunc = function (str) {
+  if (str.length < 30) {
+    return str.trim(); // В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце
+  } else return str.trim().substr(0, 29) + "..."; // Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)
+};
+testVar(myVar, strFunc);
