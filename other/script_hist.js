53a6386879b6cc3492a1a50fbeb17a009584e740
Ildar Khamzin <idl.08@mail.ru>
2025-01-19 23:43:27 +0900

    nothing, just added hints

--- other/script_hist.js
+++ other/script_hist.js
@@ -1,5 +1,7 @@
 "use strict";
-
+//
+//
+//lesson 03
 // используя if-else:
 let lang = prompt("Выберите язык:", "ru, en");
 if (lang == "ru") {
@@ -91,3 +93,25 @@ namePerson === "Артем" || namePerson === "Александр"
     ? console.log("директор")
     : console.log("преподаватель")
   : console.log("студент");
+
+//
+//
+//lesson 04
+("use strict");
+
+let myVar = "Эта строка займёт 29 символов";
+const testVar = function (varCheck, callback) {
+  return console.log(
+    typeof varCheck === "string" ? callback(varCheck) : "это не строка" // Если в качестве аргумента передана не строка - функция оповещает об этом пользователя
+  );
+};
+const strFunc = function (str) {
+  if (str.length < 30) {
+    return str.trim(); // В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце
+  } else return str.trim().substr(0, 29) + "..."; // Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)
+};
+testVar(myVar, strFunc);
+//
+//
+//
+//lesson XX
