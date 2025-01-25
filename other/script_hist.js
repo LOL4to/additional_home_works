dff98500f34070eba24856d957d0d912ed01c690
Ildar Khamzin <idl.08@mail.ru>
2025-01-26 06:14:49 +0900

    added lesson07

--- other/script_hist.js
+++ other/script_hist.js
@@ -132,4 +132,31 @@ testVar(myVar, strFunc);
 
 //
 //
-// Additional Lesson XX ---------------------
+("use strict");
+// Additional Lesson 05 ---------------------
+// 1)
+let arr = ["12", "256", "784", "41", "94", "4841", "2009"];
+let arrCheck = arr.filter(function (num) {
+  return num[0] == 2 || num[0] == 4;
+});
+console.log(arrCheck);
+
+// 2)
+// если при делении числа от 2 до 100 (varSimp) будет оставаться остаток, отличный от 0, то это true -> выводим в консоли ф-ции
+const simpleCheck = function (varSimp) {
+  for (let i = 2; i < varSimp; i++) {
+    if (varSimp % i === 0) return false; //если остаток 0, значит число составное
+  }
+  return true;
+};
+//просто вызыв ф-цию simpleCheck + выводит простые числа в консоль
+function simplePrint(varPrint) {
+  for (let i = 2; i <= varPrint; i++) {
+    if (simpleCheck(i)) console.log("Делители этого числа: 1 и " + i);
+  }
+}
+simplePrint(100);
+//
+// Additional Lesson 06 in Main Repo ---------------------
+//
+// Additional Lesson 07 ---------------------
--- script.js
+++ script.js
@@ -1,131 +1,44 @@
 "use strict";
-// Additional Lesson 02 ---------------------
-let num = 266219;
-let numHelp = 1; // для начального значения и помощи в цикле
-for (let i = 0; i < String(num).length; i++) {
-  // число num в строку, затем перебираем каждую цифру
-  numHelp *= Number(String(num)[i]); // переумножаем каждую цифру и переводим тип переменной обратно из string в number
-}
-console.log(
-  "Мой 1й способ с циклом for: " + String(numHelp ** 3).substring(0, 2)
-);
-
-// 2й способ решения задачи, с помощью метода reduce:
-let numArray = String(num).split("").map(Number); // преобразуем число в строку, разделим на массив,
-// и преобразуем обратно в число, получив массив из чисел
-let numMulti = numArray.reduce(function (initial, current) {
-  // initial = a[0]
-  return initial * current;
-});
-console.log("2й способ с reduce: " + String(numMulti ** 3).substring(0, 2));
-
-//
-//
-// Additional Lesson 03 ---------------------
-// используя if-else:
-let lang = prompt("Выберите язык:", "ru, en");
-if (lang == "ru") {
-  console.log(
-    "Понедельник\n",
-    "Вторник\n",
-    "Среда\n",
-    "Четверг\n",
-    "Пятница\n",
-    "Суббота\n",
-    "Воскресенье"
-  );
-} else if (lang == "en") {
-  console.log(
-    "Monday\n",
-    "Tuesday\n",
-    "Wednesday\n",
-    "Thursday\n",
-    "Friday\n",
-    "Saturday\n",
-    "Sunday"
-  );
-} else {
-  console.log("Выберите 'ru' или 'en'");
-}
-
-// используя switch:
-switch (lang) {
-  case "ru":
-    console.log(
-      "Понедельник\n",
-      "Вторник\n",
-      "Среда\n",
-      "Четверг\n",
-      "Пятница\n",
-      "Суббота\n",
-      "Воскресенье"
-    );
-    break;
-  case "en":
-    console.log(
-      "Monday\n",
-      "Tuesday\n",
-      "Wednesday\n",
-      "Thursday\n",
-      "Friday\n",
-      "Saturday\n",
-      "Sunday"
-    );
-    break;
-  default:
-    console.log("Выберите 'ru' или 'en'");
-}
-
-// используя многомерный массив:
-let langArr = lang.split();
-let myArr = [
-  [
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-    "Воскресенье",
-  ],
-  [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ],
+let week = [
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+  "Воскресенье",
 ];
 
-String(langArr) == ["ru"] || String(langArr).includes("en") == true // использ. 2 способа сравн для интереса
-  ? String(langArr) == ["ru"]
-    ? console.log(myArr[0])
-    : console.log(myArr[1])
-  : console.log("Выберите ru или en!");
+let myDate = new Date();
+let toDay = new Date();
 
-//----// следующая задачка
-let namePerson = prompt("Введите имя!");
-console.log(namePerson);
-namePerson === "Артем" || namePerson === "Александр"
-  ? namePerson === "Артем"
-    ? console.log("директор")
-    : console.log("преподаватель")
-  : console.log("студент");
+//т.к. отсчёт в объекте воскресенье new Date начинается с 0,
+// нужно передвинуть дни, чтобы соот-ть элементам массива
+myDate.setDate(toDay.getDay() + 6);
 
-//
-//
-// Additional Lesson 04 ---------------------
-let myVar = "Эта строка займёт 29 символов";
-const testVar = function (varCheck, callback) {
-  return console.log(
-    typeof varCheck === "string" ? callback(varCheck) : "это не строка" // Если в качестве аргумента передана не строка - функция оповещает об этом пользователя
-  );
-};
-const strFunc = function (str) {
-  if (str.length < 30) {
-    return str.trim(); // В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце
-  } else return str.trim().substr(0, 29) + "..."; // Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)
-};
-testVar(myVar, strFunc);
+//число дня недели
+let dayOfWeek = myDate.getDate();
+
+// вывод "суббота" и "воскресенье" курсивом + вывод на экран
+const myArr2 = week.findIndex(function (item) {
+  if (item === "Суббота" || item === "Воскресенье") {
+    document.write(item.italics() + "<br>");
+  } else document.write(item + "<br>");
+});
+
+// обращение к эл-ту массива и выделяем жирным текущий день
+for (let key in week) {
+  if (myDate.getDate() == key) {
+    document.write(week[key].bold());
+  }
+}
+console.log(dayOfWeek);
+
+// let newArrRow = [];
+// for (let i = 0; i < week.length; i++) {
+//   newArrRow = week + "\n";
+//   if (newArrRow == toDay) {
+//     document.write(newArrRow.bold() + "11231fgsdg" + "<br>");
+//   }
+// }
+// console.log(newArrRow);
